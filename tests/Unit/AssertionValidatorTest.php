fa6e5cb9d5d8d66f85fb80bf8dbe6e4018427f5d
Jon Cram <jon@webignition.net>
2019-07-25 15:17:48 +0100

    Update AssertionValidator

--- tests/Unit/AssertionValidatorTest.php
+++ tests/Unit/AssertionValidatorTest.php
@@ -9,9 +9,10 @@ use webignition\BasilModel\Assertion\AssertionComparisons;
 use webignition\BasilModel\Assertion\AssertionInterface;
 use webignition\BasilModel\Identifier\Identifier;
 use webignition\BasilModel\Identifier\IdentifierTypes;
-use webignition\BasilModel\Value\Value;
-use webignition\BasilModel\Value\ValueTypes;
+use webignition\BasilModel\Value\ElementValue;
+use webignition\BasilModel\Value\LiteralValue;
 use webignition\BasilModelFactory\AssertionFactory;
+use webignition\BasilModelFactory\ValueFactory;
 use webignition\BasilModelValidator\AssertionValidator;
 use webignition\BasilModelValidator\Result\InvalidResult;
 use webignition\BasilModelValidator\Result\ResultInterface;
@@ -58,38 +59,64 @@ class AssertionValidatorTest extends \PHPUnit\Framework\TestCase
     public function validateNotValidDataProvider(): array
     {
         $assertionFactory = AssertionFactory::createFactory();
+        $valueFactory = ValueFactory::createFactory();
 
-        $assertionMissingIdentifier = $assertionFactory->createFromAssertionString('');
-        $assertionWithInvalidComparison = $assertionFactory->createFromAssertionString('".selector" foo');
-        $isComparisonMissingValue = $assertionFactory->createFromAssertionString('".selector" is');
-        $isNotComparisonMissingValue = $assertionFactory->createFromAssertionString('".selector" is-not');
+        $emptyAssertion = $assertionFactory->createFromAssertionString('');
 
-        $includesComparisonMissingValue = $assertionFactory->createFromAssertionString('".selector" includes');
-        $excludesComparisonMissingValue = $assertionFactory->createFromAssertionString('".selector" excludes');
-        $matchesComparisonMissingValue = $assertionFactory->createFromAssertionString('".selector" matches');
-
-        $invalidIdentifier = new Identifier('foo', new Value(ValueTypes::STRING, ''));
-        $assertionWithInvalidIdentifier = new Assertion(
-            'foo',
-            $invalidIdentifier,
-            'foo'
+        $invalidElementValue = new ElementValue(
+            new Identifier(
+                IdentifierTypes::PAGE_ELEMENT_REFERENCE,
+                LiteralValue::createStringValue('invalid')
+            )
         );
 
-        $invalidValue = new Value('foo', '');
-        $assertionWithInvalidValue = new Assertion(
-            '".selector" is "value"',
-            new Identifier(IdentifierTypes::CSS_SELECTOR, new Value(ValueTypes::STRING, '.selector')),
+        $assertionWithInvalidExaminedValue = new Assertion(
+            '',
+            $invalidElementValue,
+            AssertionComparisons::EXISTS
+        );
+
+        $assertionWithInvalidComparison = $assertionFactory->createFromAssertionString('".selector" foo');
+        $isComparisonMissingExpectedValue = $assertionFactory->createFromAssertionString('".selector" is');
+
+        $assertionWithInvalidExpectedValue = new Assertion(
+            '',
+            $valueFactory->createFromValueString('$page.url'),
             AssertionComparisons::IS,
-            $invalidValue
+            $invalidElementValue
+        );
+
+        $assertionWithElementSelectorLiteralExpectedValue = new Assertion(
+            '',
+            $valueFactory->createFromValueString('$page.url'),
+            AssertionComparisons::IS,
+            LiteralValue::createCssSelectorValue('.selector')
+        );
+
+        $assertionWithPageElementReferenceExpectedValue = $assertionFactory->createFromAssertionString(
+            '$page.url is page_import_name.elements.element_name'
         );
 
         return [
-            'missing identifier' => [
-                'assertion' => $assertionMissingIdentifier,
+            'missing examined value' => [
+                'assertion' => $emptyAssertion,
                 'expectedResult' => new InvalidResult(
-                    $assertionMissingIdentifier,
+                    $emptyAssertion,
                     TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_IDENTIFIER_MISSING
+                    AssertionValidator::REASON_EXAMINED_VALUE_MISSING
+                ),
+            ],
+            'invalid examined value, invalid element value' => [
+                'assertion' => $assertionWithInvalidExaminedValue,
+                'expectedResult' => new InvalidResult(
+                    $assertionWithInvalidExaminedValue,
+                    TypeInterface::ASSERTION,
+                    AssertionValidator::REASON_EXAMINED_VALUE_INVALID,
+                    new InvalidResult(
+                        $invalidElementValue,
+                        TypeInterface::VALUE,
+                        ValueValidator::REASON_ELEMENT_VALUE_IDENTIFIER_INVALID
+                    )
                 ),
             ],
             'invalid comparison' => [
@@ -100,65 +127,41 @@ class AssertionValidatorTest extends \PHPUnit\Framework\TestCase
                     AssertionValidator::REASON_COMPARISON_INVALID
                 ),
             ],
-            'is comparison missing value' => [
-                'assertion' => $isComparisonMissingValue,
+            'is comparison missing expected value' => [
+                'assertion' => $isComparisonMissingExpectedValue,
                 'expectedResult' => new InvalidResult(
-                    $isComparisonMissingValue,
+                    $isComparisonMissingExpectedValue,
                     TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_VALUE_MISSING
+                    AssertionValidator::REASON_EXPECTED_VALUE_MISSING
                 ),
             ],
-            'is-not comparison missing value' => [
-                'assertion' => $isNotComparisonMissingValue,
+            'invalid examined value, expected value of correct type but invalid' => [
+                'assertion' => $assertionWithInvalidExpectedValue,
                 'expectedResult' => new InvalidResult(
-                    $isNotComparisonMissingValue,
+                    $assertionWithInvalidExpectedValue,
                     TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_VALUE_MISSING
-                ),
-            ],
-            'includes comparison missing value' => [
-                'assertion' => $includesComparisonMissingValue,
-                'expectedResult' => new InvalidResult(
-                    $includesComparisonMissingValue,
-                    TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_VALUE_MISSING
-                ),
-            ],
-            'excludes comparison missing value' => [
-                'assertion' => $excludesComparisonMissingValue,
-                'expectedResult' => new InvalidResult(
-                    $excludesComparisonMissingValue,
-                    TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_VALUE_MISSING
-                ),
-            ],
-            'matches comparison missing value' => [
-                'assertion' => $matchesComparisonMissingValue,
-                'expectedResult' => new InvalidResult(
-                    $matchesComparisonMissingValue,
-                    TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_VALUE_MISSING
-                ),
-            ],
-            'invalid identifier' => [
-                'assertion' => $assertionWithInvalidIdentifier,
-                'expectedResult' => new InvalidResult(
-                    $assertionWithInvalidIdentifier,
-                    TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_IDENTIFIER_INVALID
-                ),
-            ],
-            'invalid value' => [
-                'assertion' => $assertionWithInvalidValue,
-                'expectedResult' => new InvalidResult(
-                    $assertionWithInvalidValue,
-                    TypeInterface::ASSERTION,
-                    AssertionValidator::REASON_VALUE_INVALID,
+                    AssertionValidator::REASON_EXPECTED_VALUE_INVALID,
                     new InvalidResult(
-                        $invalidValue,
+                        $invalidElementValue,
                         TypeInterface::VALUE,
-                        ValueValidator::REASON_TYPE_INVALID
+                        ValueValidator::REASON_ELEMENT_VALUE_IDENTIFIER_INVALID
                     )
+                ),
+            ],
+            'invalid examined value, element selector literal' => [
+                'assertion' => $assertionWithElementSelectorLiteralExpectedValue,
+                'expectedResult' => new InvalidResult(
+                    $assertionWithElementSelectorLiteralExpectedValue,
+                    TypeInterface::ASSERTION,
+                    AssertionValidator::REASON_EXPECTED_VALUE_INVALID
+                ),
+            ],
+            'invalid examined value, page element reference object value' => [
+                'assertion' => $assertionWithPageElementReferenceExpectedValue,
+                'expectedResult' => new InvalidResult(
+                    $assertionWithPageElementReferenceExpectedValue,
+                    TypeInterface::ASSERTION,
+                    AssertionValidator::REASON_EXPECTED_VALUE_INVALID
                 ),
             ],
         ];
@@ -179,26 +182,53 @@ class AssertionValidatorTest extends \PHPUnit\Framework\TestCase
         $assertionFactory = AssertionFactory::createFactory();
 
         return [
-            'is comparison' => [
+            'css selector, is comparison' => [
                 'assertion' => $assertionFactory->createFromAssertionString('".selector" is "value"'),
             ],
-            'is-not comparison' => [
+            'css selector, is-not comparison' => [
                 'assertion' => $assertionFactory->createFromAssertionString('".selector" is-not "value"'),
             ],
-            'exists comparison' => [
+            'css selector, exists comparison' => [
                 'assertion' => $assertionFactory->createFromAssertionString('".selector" exists'),
             ],
-            'not-exists comparison' => [
+            'css selector, not-exists comparison' => [
                 'assertion' => $assertionFactory->createFromAssertionString('".selector" not-exists'),
             ],
-            'includes comparison' => [
+            'css selector, includes comparison' => [
                 'assertion' => $assertionFactory->createFromAssertionString('".selector" includes "value"'),
             ],
-            'excludes comparison' => [
+            'css selector, excludes comparison' => [
                 'assertion' => $assertionFactory->createFromAssertionString('".selector" excludes "value"'),
             ],
-            'matches comparison' => [
+            'css selector, matches comparison' => [
                 'assertion' => $assertionFactory->createFromAssertionString('".selector" matches "value"'),
+            ],
+            'browser object property, is comparison' => [
+                'assertion' => $assertionFactory->createFromAssertionString('$browser.size is "value"'),
+            ],
+            'data parameter, is comparison' => [
+                'assertion' => $assertionFactory->createFromAssertionString('$data.key is "value"'),
+            ],
+            'element parameter, is comparison' => [
+                'assertion' => $assertionFactory->createFromAssertionString('$elements.element_name is "value"'),
+            ],
+            'page object property, is comparison' => [
+                'assertion' => $assertionFactory->createFromAssertionString('$page.url is "value"'),
+            ],
+            'environment parameter, is comparison' => [
+                'assertion' => $assertionFactory->createFromAssertionString('$env.KEY is "value"'),
+            ],
+            'css selector, is comparison, data parameter' => [
+                'assertion' => $assertionFactory->createFromAssertionString('".selector" is $data.key'),
+            ],
+            'css selector, is comparison, element parameter' => [
+                'assertion' => $assertionFactory->createFromAssertionString('".selector" is $elements.element_name'),
+            ],
+            'css selector, is comparison, page object property' => [
+                'assertion' => $assertionFactory->createFromAssertionString('".selector" is $page.title'),
+            ],
+            'css selector, is comparison, environment parameter' => [
+                'assertion' => $assertionFactory->createFromAssertionString('".selector" is $env.KEY'),
             ],
         ];
     }
